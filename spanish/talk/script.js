f37b2a7b0458367ae9c43720997e7cdbc9921298
BenjaminGustafson <bgustafs@trinity.edu>
2020-06-16 00:18:36 -0500

    switched to gallery page

--- spanish/talk/script.js
+++ spanish/talk/script.js
@@ -1,21 +1,55 @@
 const DOM = {
   messages: document.querySelector('.messages'),//the message pane
+  prompts: document.getElementById('prompts'),
   input: document.querySelector('.message-form__input'),//stuff inside the input bar
   form: document.querySelector('.message-form'),//the input bar
 };
 
-DOM.form.addEventListener('submit', sendMessage);//when input is submitted send message
 
-function sendMessage() {
-  const value = DOM.input.value;//get text from input bar
-  if (value === '') {//if no input do nothing
-    return;
-  }
-  DOM.input.value = '';//clear input bar
-  console.log("Sending message " + value)
+
+function setPrompt(msg) {
+  prompts.innerHTML = '';
+  prompts.appendChild(document.createTextNode("Say '" + msg + "'"));
+}
+
+DOM.form.addEventListener('submit', sendInput);//when input is submitted send message
+//sendInput -> userSend -> send
+//                      -> respond -> compSend -> send
+//
+
+function sendInput() {
+  const value = DOM.input.value;
+  if (value === '') return;
+  DOM.input.value = '';
+  userSend(value);
+}
+
+function send(msg) {
   const msgs = DOM.messages;
-  msgs.appendChild(createMessageElement(value));
+  msgs.appendChild(createMessageElement(msg));
   msgs.scrollTop = msgs.scrollHeight - msgs.clientHeight;//set scroll to bottom
+}
+
+function compSend(msg) {
+  send("Computer: " + msg)
+}
+function userSend(msg) {
+  send("User: " + msg)
+  respond(msg)
+}
+
+var step = 0;
+const conv = [["hola","hola"],["como estas","bien"]];
+setPrompt(conv[0][0]);
+
+function respond(msg) {
+  if(step < conv.length && conv[step][0] == msg){
+    compSend(conv[step][1]);
+    step++;
+    setPrompt(conv[step][0])
+  }else{
+    // end conversation?
+  }
 }
 
 function createMessageElement(text) {
