74a4c30c34bb06a204227fe12eb61f183081144c
BenjaminGustafson <bgustafs@trinity.edu>
2022-01-22 15:27:53 -0600

    remove files

--- assets/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="grass" tilewidth="16" tileheight="16" tilecount="22" columns="11">
- <image source="grass.png" width="176" height="32"/>
-</tileset>
--- assets/main.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="main" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="my_tileset.png" width="320" height="320"/>
-</tileset>
