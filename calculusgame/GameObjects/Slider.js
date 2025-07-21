12ea4b379f299e0acb8cfb8c043dca2de578c230
BenjaminGustafson <bengustafson2@gmail.com>
2025-07-21 12:31:35 -0500

    rewrite slider

--- calculusgame/GameObjects/Slider.js
+++ calculusgame/GameObjects/Slider.js
@@ -8,158 +8,195 @@ class Slider{
 
     /**
      * 
-     * TODO: refactor to not use positional optional args
-     * And specify with maxVal rather than axis.
+     * @param {Object} config
+     * @param {number} config.canvasX - x-coordinate (left if horizontal)
+     * @param {number} config.canvasY - y-coordinate (top if vertical)
+     * @param {number} config.canvasLength - The length in pixels 
+     * @param {number} config.sliderLength - The length in slider value 
      * 
-     * Sliders values example: 
-     *
-     * _____ i=0 value = 2   y = y 
-     *   |
-     * __|__ i=1 value = 1   y = y + unitLength
-     *   |
-     * <_|_> i=2 value = 0  axis = 2
-     *   |
-     * __|__ i=3 value = -1
-     *
-     * value = axis - i
-     * y = top_y + unitLength * i 
-     *   = top_y + unitLength * (axis - value)
-     * 
-     * value = axis - (y - top_y)/unitLength
-     * 
-     * @param {*} originX x-value of middle of slider (vertical)
-     * @param {*} originY y-value of top of slider (vertical)
-     * @param {*} length 
-     * @param {*} numDivision The slider will have numDivision+1 tick marks.
-     * @param {*} startValue 
-     * @param {*} axis Where 0 is on the slider. Counting from the top down starting at zero.
-     * @param {*} increment 
-     * @param {*} visible 
-     * @param {*} vertical 
-     * @param {*} circleRadius 
      */
-    constructor(originX, originY, length, numDivision, startValue,  axis, increment = 0.1, visible = true, vertical = true, circleRadius=15){
-        this.lineWidth = 5
+    constructor({
+        canvasX, canvasY,
+        grid, gridPos,
+        minValue, maxValue,
+        canvasLength = 400,
+        sliderLength = 4,
+        startValue = 0,
+        increment = 0.1,
+        circleRadius=15,
+        vertical = true,
+        showLines = true, 
+        showAxis = false,
+        circleColor = Color.red,
+        lineWidth = 5
+    }){
+        Object.assign(this, {
+            canvasLength, sliderLength, startValue, increment, circleRadius,
+            vertical, showLines, showAxis, circleColor, lineWidth  
+        })
+
+        if (canvasX != null && canvasY != null){
+            this.canvasX = canvasX
+            this.canvasY = canvasY
+
+            if (maxValue != null){
+                this.maxValue = maxValue
+                this.minValue = maxValue - sliderLength
+            }else if (minValue != null){
+                this.minValue = minValue
+                this.maxValue = minValue + sliderLength
+            }else { // default to center slider
+                this.minValue = - Math.floor(sliderLength/2)
+                this.maxValue = Math.ceil(sliderLength/2)
+            }
+    
+        }else if (grid != null && gridPos != null){
+            this.showLines = false
+            if (vertical){
+                this.canvasX = grid.gridToCanvasX(gridPos)
+                this.canvasY = grid.canvasY
+                this.canvasLength = grid.canvasHeight
+                this.sliderLength = grid.gridHeight
+                this.maxValue = grid.gridYMax
+                this.minValue = grid.gridYMin
+                console.log(this.canvasX, this.canvasY, this.minValue, this.maxValue, this.sliderLength, this.canvasLength)
+            }else{
+                this.canvasX = grid.canvasX
+                this.canvasY = grid.gridToCanvasY(gridPos)
+                this.canvasLength = grid.canvasWidth
+                this.sliderLength = grid.gridWidth
+                this.maxValue = grid.gridXMax
+                this.minValue = grid.gridXMin
+            }
+        }else{
+            throw new Error("Must provide either (canvasX, canvasY) or (grid, gridPos)")
+        }
+
+        this.unitLength = this.canvasLength / this.sliderLength
+        if (this.vertical){
+            this.axis = this.maxValue
+        }else{
+            this.axis = -this.minValue
+        }
+
+        // Dynamic vars
         this.active = true
-        this.circleColorActive = Color.red
-        this.circleColor = Color.red
-        this.originX = originX
-        this.originY = originY
-        this.length = length
-        this.numDivision = numDivision
+        this.hidden = false
         this.grabbed = false
-        this.grab_pos = 0
-        this.visible = visible
-        this.increment = increment
-        this.circleRadius = circleRadius
-        this.vertical = vertical
+        this.grabPos = 0
+        this.value = startValue
 
-        if (axis == -1 || axis == null){
-            this.axis = this.numDivision
-            this.showAxis = false
-        }else{
-            this.axis = axis
-            this.showAxis = true
-        }
-        this.value = startValue // The true value of the slider
-        this.unitLength = this.length/this.numDivision
-        // The circle goes at the tick mark given by the value
-        if (this.vertical){
-            this.circle_pos = this.originY + (this.axis - this.value) * this.unitLength
-        }else{
-            this.circle_pos = this.originX - (this.axis - this.value)* this.unitLength
-        }
-
-        
-        
-        if (this.vertical){
-            this.end_x = originX
-            this.end_y = originY + length
-            this.maxVal = axis
-            this.minVal = axis-numDivision
-        }else{
-            this.end_x = originX + length
-            this.end_y = originY
-            this.maxVal = numDivision-axis // Definitely bugged, but works if axis = 0
-            this.minVal = axis
-        }
-        
+        this.updateCircle()
     }
 
+    valueToCanvas(val){
+        if (this.vertical){
+            return this.canvasY + (this.maxValue - val) * this.unitLength
+        }else{
+            return this.canvasX + (val - this.minValue) * this.unitLength
+        }
+    }
+
+    canvasToValue(cVal){
+        if (this.vertical){
+            return - (cVal-this.canvasY) / this.unitLength + this.maxValue
+        }else {
+            return (cVal - this.canvasY) / this.unitLength + this.minValue
+        }
+    }
+
+    /**
+     * Given a canvas value returns a valid slider value.
+     * 
+     * @param {number} cVal - the canvas value (y for vertical, x for horizontal)
+     * @returns - the slider value
+     */
+    canvasToValueBounded(cVal){
+        var value = this.canvasToValue(cVal)
+        value = Math.min(this.maxValue, Math.max(this.minValue, value)) 
+        value = Math.round(value/this.increment)*this.increment
+        return value
+    }
+
+    updateCircle(){
+        this.circlePos = this.valueToCanvas(this.value) 
+    }
+
+    /**
+     * Guarded function to set the value of the slider.
+     * @param {number} val 
+     */
     setValue(val){
-        console.log('value',this.minVal,this.maxVal,val)
-        if (val < this.minVal) val = this.minVal
-        if (val > this.maxVal) val = this.maxVal
+        if (val < this.minValue) val = this.minValue
+        if (val > this.maxValue) val = this.maxValue
         this.value = val
-        if (this.vertical){
-            this.circle_pos = this.originY + (this.axis - this.value)* this.unitLength// TODO duplicate code
-        }else{
-            this.circle_pos = this.originX  - (this.axis - this.value)* this.unitLength
-        }
+        this.updateCircle()
     }
 
-    draw(ctx, audioManager){
+    draw(ctx){
         if (this.hidden){
             return
         }
-        if (this.active){
-            this.circleColor = this.circleColorActive
-        }else{
-            this.circleColor = Color.gray
-        }
 
-        if (this.playSound){
-            console.log('Playing')
-            audioManager.play('click')
-            this.playSound = false
-        }
-
-        if(this.lineWidth != -1){
-
+        // Draw line and tick marks
+        if (this.showLines){
             Color.setColor(ctx, Color.white)
-            if (this.visible){
-                Shapes.RoundedLine(ctx, this.originX, this.originY, this.end_x, this.end_y, this.lineWidth)
+            if (this.vertical){
+                Shapes.RoundedLine(ctx, this.canvasX, this.canvasY, this.canvasX, this.canvasY + this.canvasLength, this.lineWidth)
+            }else{
+                Shapes.RoundedLine(ctx, this.canvasX, this.canvasY, this.canvasX + this.canvasLength, this.canvasY, this.lineWidth)
+            }
 
-                for (let i = 0; i <= this.numDivision; i++){
-                    const crossLength = 15
-                    const lineWidth = this.lineWidth
-                    var value = this.axis - i
-                    if (!this.vertical){
-                        value = -this.axis + i
-                    }
-                    // If we are on a tick, color it
-                    if (value == this.value && !this.grabbed){
-                        Color.setColor(ctx, this.circleColor)
-                    }else{
-                        Color.setColor(ctx, Color.white)
-                    }
-                    // Draw 0 tick with arrows
-                    var lineType = "rounded"
-                    if (this.showAxis == true && i == this.axis){
-                        lineType = "arrow"
-                    }
-                    // Tick marks
-                    if (this.vertical){
-                        Shapes.Line(ctx, this.originX-crossLength, this.originY+i*this.unitLength,
-                        this.originX+crossLength, this.originY+i*this.unitLength, 
-                        lineWidth, lineType)
-                    }else{
-                        Shapes.Line(ctx, this.originX+i*this.unitLength, this.originY -crossLength,
-                            this.originX+i*this.unitLength, this.originY+crossLength, 
-                            lineWidth, lineType)
-                    }
+            for (let i = 0; i <= this.sliderLength; i++){
+                const tickLength = 15
+                const lineWidth = this.lineWidth
+                var val = this.vertical ? this.maxValue - i : this.minValue + i
+
+                // If we are on a tick, color it
+                if (val == this.value && !this.grabbed && this.active){
+                    Color.setColor(ctx, this.circleColor)
+                }else{
+                    Color.setColor(ctx, Color.white)
+                }
+                // Draw 0 tick with arrows
+                var lineType = "rounded"
+                if (this.showAxis && i == this.axis){
+                    lineType = "arrow"
+                }
+                // Tick marks
+                if (this.vertical){
+                    Shapes.Line(ctx,
+                        this.canvasX-tickLength, this.canvasY+i*this.unitLength,
+                        this.canvasX+tickLength, this.canvasY+i*this.unitLength, 
+                        lineWidth, lineType
+                    )
+                }else{
+                    Shapes.Line(ctx, 
+                        this.canvasX+i*this.unitLength, this.canvasY-tickLength,
+                        this.canvasX+i*this.unitLength, this.canvasY+tickLength, 
+                        lineWidth, lineType
+                    )
                 }
             }
         }
 
         // Draw slider handle (circle)
-        Color.setColor(ctx, this.circleColor)
+        Color.setColor(ctx, this.active ? this.circleColor : Color.gray)
         if (this.vertical){
-            Shapes.Circle(ctx, this.originX,this.circle_pos, this.circleRadius)
+            Shapes.Circle(ctx, this.canvasX, this.circlePos, this.circleRadius)
         }else{
-            Shapes.Circle(ctx, this.circle_pos,this.originY, this.circleRadius)
+            Shapes.Circle(ctx, this.circlePos, this.canvasY, this.circleRadius)
         }
-        ctx.resetTransform()
+    }
+
+
+    mouseOverCircle(x,y){
+        const square = x => x * x;
+        if (this.vertical){
+            return square(this.canvasX - x) + square(this.circlePos- y) <= square(this.circleRadius)
+        } else {
+            return square(this.canvasY - y) + square(this.circlePos - x) <= square(this.circleRadius)
+        }
     }
 
     mouseMove(x,y){
@@ -167,43 +204,29 @@ class Slider{
             return null
         }
         if (this.grabbed){
-            
-            this.circle_pos = Math.max(Math.min(y - this.grab_pos, this.originY+this.length), this.originY)
-            var value =  this.axis - Math.round((this.circle_pos-this.originY)/this.unitLength/this.increment)*this.increment
-            if (!this.vertical){
-                this.circle_pos = Math.max(Math.min(x - this.grab_pos, this.originX+this.length), this.originX)
-                value = - this.axis + Math.round((this.circle_pos-this.originX)/this.unitLength/this.increment)*this.increment
+            const newValue = this.canvasToValueBounded(this.vertical ? y : x)
+            if (newValue != this.value){
+                this.value = newValue
             }
-            if (value != this.value){
-                this.playSound = true
-                this.value = value
-            }
-            if (this.vertical){
-                this.circle_pos = this.originY + (this.axis - this.value)* this.unitLength
-            }else{
-                this.circle_pos = this.originX - (this.axis - this.value)* this.unitLength
-            }
+            this.updateCircle()
             return 'grabbing'
         }
-        if ((this.vertical && (this.originX - x)*(this.originX - x) + (this.circle_pos - y)*(this.circle_pos- y) <= this.circleRadius*this.circleRadius)
-        || (!this.vertical && (this.originY - y)*(this.originY - y) + (this.circle_pos - x)*(this.circle_pos- x) <= this.circleRadius*this.circleRadius)){
+        if (this.mouseOverCircle(x,y)){
             return 'grab'
         }
         return null
     }
 
     mouseDown(x,y){
-        //new Audio('audio/click_001.ogg').play();
         if (!this.active){
             return null
         }
-        if ((this.vertical && (this.originX - x)*(this.originX - x) + (this.circle_pos - y)*(this.circle_pos- y) <= this.circleRadius*this.circleRadius)
-            || (!this.vertical && (this.originY - y)*(this.originY - y) + (this.circle_pos - x)*(this.circle_pos- x) <= this.circleRadius*this.circleRadius)){
+        if (this.mouseOverCircle(x,y)){
             this.grabbed = true
             if (this.vertical){
-                this.grab_pos = y - this.circle_pos
+                this.grabPos = y - this.circlePos
             }else{
-                this.grab_pos = x - this.circle_pos
+                this.grabPos = x - this.circlePos
             }
             return 'grabbing'
         }
@@ -217,14 +240,10 @@ class Slider{
             return null
         }
         this.grabbed = false
-        if ((this.vertical && (this.originX - x)*(this.originX - x) + (this.circle_pos - y)*(this.circle_pos- y) <= this.circleRadius*this.circleRadius)
-            || (!this.vertical && (this.originY - y)*(this.originY - y) + (this.circle_pos - x)*(this.circle_pos- x) <= this.circleRadius*this.circleRadius)){
+        if (this.mouseOverCircle(x,y)){
             return 'grab'
         }
         return null
-        //new Audio('audio/click_001.ogg').play();
-        // this.value = Math.round((this.circle_pos-this.originY)/this.length*this.numDivision)
-        // this.circle_y = this.originY + this.value* this.length/this.numDivision
     }
 
     solved(){
