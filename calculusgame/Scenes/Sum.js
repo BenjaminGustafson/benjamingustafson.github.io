192ff91cb09e79da756a294cea5a7da134fa613d
BenjaminGustafson <bengustafson2@gmail.com>
2025-09-09 19:29:45 -0500

    create discrete sum levels

--- calculusgame/Scenes/Sum.js
+++ calculusgame/Scenes/Sum.js
@@ -15,6 +15,7 @@ const nodes = {
     'sum.puzzle.2': [7,1, 0,-1],
     'sum.puzzle.3': [8,1, 0,-1],
     'sum.puzzle.4': [9,1, 0,-1],
+    'sum.dialogue.1':[9,3, 0,1],
     'sum.puzzle.5': [10,1, 0,-1],
     'sum.puzzle.6': [11,1, 0,-1],
     'sum.puzzle.7': [12,1, 0,-1],
@@ -31,6 +32,7 @@ const paths =
     {start: 'sum.puzzle.2', end: 'sum.puzzle.3', steps: [] },
     {start: 'sum.puzzle.3', end: 'sum.puzzle.4', steps: [] },
     {start: 'sum.puzzle.4', end:  'sum.puzzle.5', steps: [] },
+    {start: 'sum.puzzle.4', end:  'sum.dialogue.1', steps: [] },
     {start: 'sum.puzzle.5', end: 'sum.puzzle.6', steps: [] },
     {start: 'sum.puzzle.6', end: 'sum.puzzle.7', steps: [] },
     {start: 'sum.puzzle.7', end: 'sum.puzzle.8', steps: [] },
@@ -59,11 +61,17 @@ export function loadScene(gameState, sceneName, message = {}){
     switch(sceneNameSplit[1]){
         case "puzzle": 
             switch(sceneNameSplit[2]){
+                // case '1':
+                //     sumSliderLevel(gameState, {numSliders:4, sliderSize:15, gridYMin:-2, gridYMax:2,gridXMin:-2,gridXMax:2,
+                //         magentaTargetValues: [1,1,1,1], yellowTargetValues : [-1,-0.5,0,0.5],
+                //         greenTracerStart:-1.5, redTracerStart:1,
+                //          nextScenes:["sum.puzzle.2"]})
+                //     break
                 case '1':
-                    sumSliderLevel(gameState, {numSliders:4, sliderSize:15, gridYMin:-2, gridYMax:2,gridXMin:-2,gridXMax:2,
-                        magentaTargetValues: [1,1,1,1], yellowTargetValues : [-1,-0.5,0,0.5],
-                        greenTracerStart:-1.5, redTracerStart:1,
-                         nextScenes:["sum.puzzle.2"]})
+                    sumSliderLevel(gameState, {numSliders:1, sliderSize:15, gridYMin:-2, gridYMax:2,gridXMin:0,gridXMax:1,
+                        magentaTargetValues: [1], yellowTargetValues : [-0.5],
+                        greenTracerStart:-1, redTracerStart:0, whiteTracerStart:1,
+                            nextScenes:["sum.puzzle.2"]})
                     break
 
                 case '2':
@@ -82,22 +90,24 @@ export function loadScene(gameState, sceneName, message = {}){
                     sumSliderLevel(gameState, {numSliders:4, sliderSize:15, gridYMin:-2, gridYMax:2,gridXMin:-2,gridXMax:2,
                         magentaTargetValues: [0.5,1,0.5,0], yellowTargetValues : [-0.5,-1,-0.5,-1],
                         greenTracerStart:0, redTracerStart:0,whiteTracerStart:0,
-                            nextScenes:["sum.puzzle.4"]})
+                            nextScenes:["sum.puzzle.5", 'sum.dialogue.1']})
+                    break
+                case '5':
+                    sumMathBlockLevel(gameState, {numSliders:4, sliderSize:15, gridYMin:-2, gridYMax:2,gridXMin:-2,gridXMax:2,
+                        magentaTargetValues: [0.5,1,0.5,0], yellowTargetValues : [-0.5,-1,-0.5,-1],
+                        greenTracerStart:0, redTracerStart:0,whiteTracerStart:0,
+                            nextScenes:["sum.puzzle.6"]})
                     break
             }
         break
 
         case 'dialogue':
             sumPlanet(gameState)
-            switch(sceneNameSplit[2]){
-                case '1':
-                    dialogueScene(gameState, {exitTo:"sum", nextScenes:["sum.puzzle.5"], text: [    
-                        "⯘Ⳃⱙⰺⳡ ⰺⳝ⯨⯃⯎ ⱤⳆⰸ⯃ ⳙ⯹ⱡ ⯷ⳞⳤⱭⰶ.",
-                        "ⳏⳐⰷ⯁Ⱨⰴ ⯢ⱋⳒⰳⳙ ⯚⯜⯍ ⳙⰿⱆ ⳨⯟ⳑ⳪⳰ ⰴⱢⳈⳡ ⱍ⳧Ⳑⰿ.",
-                        "ⳟ⯔ ⳓ⯥ⱄⰳ ⳉⳂⳙ⯎ ⱤⳆⰸ⯃ Ɀⰳⱅⰸⳝ ⯢ⳔⳂⳚ ⱇⱏⰴⳂ ⰳⳤⱑ⯅ⰴ!"
-                    ]})
-                break
-            }
+            case '1':
+                Planet.dialogueScene(gameState, {nextScenes:["linear.puzzle.5"], text: [
+                    'The red line is the sum of the green and white lines.',
+                    'The slope of red is the slope of green plus the slope of white.'
+                ]})
             break
 
         case "lab":
@@ -130,115 +140,78 @@ function sumPlanet(gameState,message){
 }
 
 
-function sumLevel (gameState, {
+
+function sumMathBlockLevel (gameState, {
     numSliders,
-    withMathBlock = false,
-    tracerStart=0,
+    whiteTracerStart=0,
+    redTracerStart=0,
+    greenTracerStart=0,
     targetSize = 20, sliderSize = 15,
     nextScenes, 
-    gridXMax=5,
+    gridXMax=2,
     gridYMax=2,
     gridYMin=-2,
-    gridXMin=-5,
+    gridXMin=-2,
     increment=0.1,
-    nSliderMin=0,nSliderMax=5,nSliderIncrement=1,
-    fun1, fun2,
+    magentaTargetValues, 
+    yellowTargetValues,
 }){
     const gss = gameState.stored
     const backButton = Planet.backButton(gameState)
     const nextButton = Planet.nextButton(gameState, nextScenes)
 
-    const gridLeft = new Grid({canvasX:50, canvasY:350, canvasWidth:400, canvasHeight:400, 
+    // Grids
+    const gridLeft = new Grid({canvasX:150, canvasY:350, canvasWidth:400, canvasHeight:400, 
         gridXMin:gridXMin, gridYMin:gridYMin, gridXMax:gridXMax, gridYMax:gridYMax, labels:true, arrows:false, autoCellSize: true})
-    const gridMiddle = new Grid({canvasX:500, canvasY:350, canvasWidth:400, canvasHeight:400, 
-        gridXMin:gridXMin, gridYMin:gridYMin, gridXMax:gridXMax, gridYMax:gridYMax, labels:true, arrows:false, autoCellSize: true
-    })
-    const gridRight = new Grid({canvasX:950, canvasY:350, canvasWidth:400, canvasHeight:400, 
+    const gridRight = new Grid({canvasX:500, canvasY:350, canvasWidth:400, canvasHeight:400, 
         gridXMin:gridXMin, gridYMin:gridYMin, gridXMax:gridXMax, gridYMax:gridYMax, labels:true, arrows:false, autoCellSize: true})
     
-    const spacing = gridLeft.gridWidth/numSliders
-    var greenSliders = []
-    var whiteSliders = []
-    for (let i = 0; i < numSliders; i++){
-        greenSliders.push(new Slider({grid:gridMiddle, gridPos:gridMiddle.gridXMin + i * spacing,
-            increment: increment,circleRadius:sliderSize,circleColor:new Color(0,200,50)}))
-        whiteSliders.push(new Slider({grid:gridRight, gridPos:gridRight.gridXMin + i * spacing,
-            increment: increment,circleRadius:sliderSize,circleColor:new Color(200,200,200)}))
-    }
+    // Function tracers
+    const greenFunTracer = new FunctionTracer({grid: gridRight})
     
-
+    // Targets
     var magentaTargets = []
     var yellowTargets = []
     for (let i = 0; i < numSliders; i++) {
         const x = gridLeft.gridXMin+(i+1)*spacing
-        const y1 = fun1(x)
-        if (gridLeft.isInBoundsGridY(y1))
-            yellowTargets.push(new Target({grid: gridLeft, gridX:x, gridY:y1, size:targetSize,unhitColor:Color.yellow}))
-
-        const y = fun1(x) + fun2(x)
-        if (gridLeft.isInBoundsGridY(y))
-            magentaTargets.push(new Target({grid: gridLeft, gridX:x, gridY:y, size:targetSize}))
+        yellowTargets.push(new Target({grid: gridLeft, gridX:x, gridY:yellowTargetValues[i], size:targetSize,unhitColor:Color.yellow}))
+        magentaTargets.push(new Target({grid: gridLeft, gridX:x, gridY:magentaTargetValues[i], size:targetSize}))
     }
     
-
+    // Tracers
     const redTracer = new IntegralTracer({grid: gridLeft, sliders: whiteSliders, sliders2:greenSliders, type:'sumOfSliders',
-         targets:magentaTargets, originGridY:tracerStart
+         targets:magentaTargets, originGridY:redTracerStart
     })
-    const whiteTracer = new IntegralTracer({grid: gridLeft, sliders: whiteSliders, originGridY:tracerStart, 
+    const whiteTracer = new IntegralTracer({grid: gridLeft, input: {}, originGridY:whiteTracerStart, 
         unsolvedColor:Color.white,
     })
-    const greenTracer = new IntegralTracer({grid: gridLeft, sliders: greenSliders, targets:yellowTargets, originGridY:tracerStart,
-        unsolvedColor:Color.green,
+    const greenTracer = new IntegralTracer({grid: gridLeft, sliders: greenSliders, targets:yellowTargets, originGridY:greenTracerStart,
+        unsolvedColor:Color.green, audioChannel:1,
+    })
+
+    // Mathblocks
+    const sySlider = new Slider({canvasX: 1400, canvasY: 350, maxValue:2, sliderLength:4, startValue: 1, showAxis:true})
+    const tySlider = new Slider({canvasX: 1480, canvasY: 350, maxValue:2, sliderLength:4, showAxis:true})
+    const nSlider = new Slider({canvasX: 1560, canvasY: 350, maxValue:nSliderMax, sliderLength:nSliderMax-nSliderMin,
+         increment:nSliderIncrement, showAxis:true})
+    const mbField = new MathBlockField({minX:950, minY:100, maxX:1350, maxY:300, outputSliders:sliders})
+
+    const mbm = new MathBlockManager({blocks : blocks, toolBarX: 1400, toolBarY:150, outputType:"sliders",
+        scaleYSlider: sySlider, translateYSlider:tySlider, numSlider:nSlider,
+        blockFields: [ mbField ],
     })
     
-    const blocks = [
-        new MathBlock({type:MathBlock.VARIABLE, token:"x"}),
-    ]
-    // for (let b of gss.mathBlocksUnlocked){
-    //     blocks.push(new MathBlock({type: b.type, token: b.token}))
-    // }
+
+
 
     gameState.objects = [gridLeft, gridMiddle, gridRight, greenTracer, whiteTracer, redTracer,
          backButton, nextButton].concat(yellowTargets).concat(magentaTargets).concat(greenSliders).concat(whiteSliders)
     gameState.update = ()=> {
     }
 
-    // if (withMathBlock){
-
-    //     sliders.forEach(s => s.clickable = false)
-
-    //     const sySlider = new Slider({canvasX: 1400, canvasY: 350, maxValue:2, sliderLength:4, startValue: 1, showAxis:true})
-    //     const tySlider = new Slider({canvasX: 1480, canvasY: 350, maxValue:2, sliderLength:4, showAxis:true})
-    //     const nSlider = new Slider({canvasX: 1560, canvasY: 350, maxValue:nSliderMax, sliderLength:nSliderMax-nSliderMin,
-    //          increment:nSliderIncrement, showAxis:true})
-    //     const mbField = new MathBlockField({minX:950, minY:100, maxX:1350, maxY:300, outputSliders:sliders})
-
-    //     const mbm = new MathBlockManager({blocks : blocks, toolBarX: 1400, toolBarY:150, outputType:"sliders",
-    //         scaleYSlider: sySlider, translateYSlider:tySlider, numSlider:nSlider,
-    //         blockFields: [ mbField ],
-
-    //     })
-    //     gameState.objects = gameState.objects.concat([mbm, sySlider, tySlider, nSlider])
-        
-    //     const update = gameState.update
-    //     gameState.update = ()=>{
-    //         update()
-    //         if (mbField.rootBlock != null){
-    //             const fun = mbField.rootBlock.toFunction()
-    //             if (fun != null){
-    //                 for (let i = 0; i < numSliders; i++){
-    //                     sliders[i].moveToValue(fun(sliders[i].gridPos))
-    //                     //sliders[i].setValue(fun(sliders[i].gridPos))
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    Planet.winCon(gameState, ()=>redTracer.solved, nextButton)
+    Planet.winCon(gameState, ()=>redTracer.solved&&greenTracer.solved, nextButton)
     Planet.unlockScenes(nextScenes, gss)
 }
-
 
 
 function sumSliderLevel (gameState, {
@@ -287,16 +260,16 @@ function sumSliderLevel (gameState, {
     }
     
 
-    const redTracer = new IntegralTracer({grid: gridLeft, sliders: whiteSliders, sliders2:greenSliders, type:'sumOfSliders',
+    const redTracer = new IntegralTracer({grid: gridLeft, 
+        input:{type:'comboSliders', sliders1:whiteSliders, sliders2:greenSliders, combo: (x,y)=>x+y},
          targets:magentaTargets, originGridY:redTracerStart
     })
-    const whiteTracer = new IntegralTracer({grid: gridLeft, sliders: whiteSliders, originGridY:whiteTracerStart, 
+    const whiteTracer = new IntegralTracer({grid: gridLeft, input:{type:'sliders', sliders: whiteSliders}, originGridY:whiteTracerStart, 
         unsolvedColor:Color.white,
     })
-    const greenTracer = new IntegralTracer({grid: gridLeft, sliders: greenSliders, targets:yellowTargets, originGridY:greenTracerStart,
-        unsolvedColor:Color.green, audioChannel:1,
+    const greenTracer = new IntegralTracer({grid: gridLeft, input:{type:'sliders', sliders: greenSliders}, targets:yellowTargets, originGridY:greenTracerStart,
+        unsolvedColor:Color.green,
     })
-    
 
     gameState.objects = [gridLeft, gridMiddle, gridRight, greenTracer, whiteTracer, redTracer,
          backButton, nextButton].concat(yellowTargets).concat(magentaTargets).concat(greenSliders).concat(whiteSliders)
