a008b17b6d58053a39170a98f258e92c4ac862f3
BenjaminGustafson <bengustafson2@gmail.com>
2025-10-07 18:17:14 -0500

    delete old calc game, moved to its own repo

--- calculusgame2/GameObjects/GameObject.js
+++
@@ -1,5 +0,0 @@
-export class GameObject {
-    hidden = false // When hidden, the objects update function is not called
-    noInput = false // When true, object recieves no input
-    update(ctx, audioManager, mouse) { throw new Error("update() must be implemented");  }
-}
--- calculusgame2/util/index.js
+++
@@ -1,3 +0,0 @@
-export {Color} from './Color.js'
-export {AudioManager} from './AudioManager.js'
-export {Shapes} from './Shapes.js'
